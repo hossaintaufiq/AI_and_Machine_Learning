d0001855c6084f6e00fded6f32c48a00ded8ad53
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-02-26 17:14:40 +0600

    python

---
+++ Python3/function.py
@@ -0,0 +1,10 @@
+x= "awsome"
+
+def myfunc(): 
+    x= "fantastic"
+    print("Question->01")
+    print("Python is "+x)
+
+myfunc()
+print("Question->02")
+print("Python is "+x)
---
+++ Python3/variable.py
@@ -0,0 +1,23 @@
+# x=5
+# y='jhon'
+
+# print(x)
+# print(y)
+
+#type casting 
+x=str(3)
+y=int(3)
+z=float(3)
+
+print(x)
+print(y)
+print(z)
+
+a = "Python is a programming language"
+print(a)
+
+b='python'
+c= 'is'
+d='awsome'
+
+print(b,c,d)
