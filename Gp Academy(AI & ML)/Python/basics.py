63cc3fef40fffdbd2f2240cc1f5e81cf4fbcfdb5
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-05-24 14:41:53 +0600

    numpy pandas and matplot lib

--- Gp Academy(AI & ML)/Python/basics.py
+++ Gp Academy(AI & ML)/Python/basics.py
@@ -7,16 +7,55 @@
 #     print("Five is less than two!")
 
 
-x=0; 
+# x=0; 
 
-if 5>2: 
-    x=5; 
+# if 5>2: 
+#     x=5; 
 
-elif 5<2: 
-    x=10; 
+# elif 5<2: 
+#     x=10; 
 
-else: 
-    x=0; 
+# else: 
+#     x=0; 
 
 
-print("The value of x is : ", x*5); 
+# print("The value of x is : ", x*5, " and the value of x is : ", x*10);
+
+# x,y,z= " Orange", "Banana", "Cherry"; 
+
+# print(x,y,z)
+
+
+# functions
+
+# x=10; 
+
+# def sumOfNumbers(): 
+#     i=0; 
+#     sum=0;
+#     while i<10:
+#         sum+=i;
+#         i+=1;
+#     return sum;
+
+
+   
+
+# print(sumOfNumbers()); 
+
+# def myFunction():
+#     print("Hello from a function")
+
+
+# # print(myFunction()); 
+# myFunction();    
+
+# import datetime
+# x=datetime.datetime.now(); 
+# print(x)
+
+# input 
+
+print("Enter your name: ")
+name=input(); 
+print("Hello ", name)
