b745568655e6fae22a67ee1198f6180c66bba142
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2026-05-24 14:02:40 +0600

    python for ml

---
+++ Gp Academy(AI & ML)/Python/basics.py
@@ -0,0 +1,22 @@
+# print("Hello world!")
+# if 5>2: 
+#     print("Five is greater than two!")
+# elif 5==2:
+#     print("Five is equal to two!")
+# else:
+#     print("Five is less than two!")
+
+
+x=0; 
+
+if 5>2: 
+    x=5; 
+
+elif 5<2: 
+    x=10; 
+
+else: 
+    x=0; 
+
+
+print("The value of x is : ", x*5); 
